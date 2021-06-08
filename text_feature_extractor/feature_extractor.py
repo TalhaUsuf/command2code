cd7d897271afa902fde29b7282c17aa4f48055b2
Talha Yousuf <talha.yousuf@stech.ai>
2021-06-08 15:13:38 +0500

    added gpt2 tokenizer

--- text_feature_extractor/feature_extractor.py
+++ text_feature_extractor/feature_extractor.py
@@ -1,14 +1,26 @@
 from transformers import GPT2TokenizerFast
 import argparse
+from rich.console import Console
 
 
 arg = argparse.ArgumentParser()
-arg.add_argument('-g','--gpt2', action="store_true", help="if use gpt2 tokenizer to get feature")
+arg.add_argument('-g', '--gpt2', action="store_true", help="if used gpt2 flag then gpt2 tokenizer to get feature")
+arg.add_argument('-t', '--tfidf', action="store_true", help="if flag given then tfidf features will be extracted")
+arg.add_argument('-b' '--bert', action="store_true", help="if flag given then bert will used as tokenizer")
+arg.add_argument('-k' '--keras', action="store_true", help="if flag given then keras will used as tokenizer")
+
 parser = arg.parse_args()
 
 
 if parser.gpt2:
-    print(f"USE GPT2")
-    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-    print([k for k in dir(tokenizer) if not k.startswith("_")])
+    Console().print(f"[red]GPT2 will be used as tokenizer[/red]")
+    tok = GPT2TokenizerFast.from_pretrained('gpt2')
+    # tok._batch_encode_plus()
+elif parser.tfidf:
+    pass
 
+elif parser.bert:
+    pass
+
+elif parser.keras:
+    pass
