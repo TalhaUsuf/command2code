80d699829e38a7f53b0249d0b230774301b8144b
Talha Yousuf <talha.yousuf@stech.ai>
2021-06-09 14:24:09 +0500

    added html docs for text_feature_extractor module

---
+++ text_feature_extractor/docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,28 @@
+.. feature extractiontext documentation master file, created by
+   sphinx-quickstart on Wed Jun  9 14:18:53 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to feature extractiontext's documentation!
+==================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+.. automodule:: feature_extractor
+   :members:
+
+
+.. automodule:: text_utils
+   :members:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- text_feature_extractor/feature_extractor.py
+++ text_feature_extractor/feature_extractor.py
@@ -11,18 +11,54 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import joblib
 from text_utils import read_file, get_tokenizer, get_args
-
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 def main():
+    """
+    gets parsers according to the flag given by user and saves the tokenizers and label encoders accordingly
+
+    Tokenizers Supported
+    --------------------
+    tfidf_tokenizer
+        uses sklearn pipeline
+    keras_tokenizer
+        uses tf.keras processing functions. The features are 0 padded to max-len in batch
+    gpt2_tokenizer
+        uses transformers library, currently dysfunctional
+    bert_tokenizer
+        uses transformers library, saves features with dictionary having following keys : 'input_ids'\
+        'token_type_ids' and 'attention_mask'
+
+    Returns
+    -------
+    None :
+        Saves the pickle files inside ``command2code`` dir.
+    """
     # parse and get tokenizer
     parser = get_args()
     tokenizer = get_tokenizer(parser)
     x, y_major, y_minor = read_file(pth="dataset/Ultimus Work/Commands_with_labels.csv")  # all are arrays
 
+    # 1) encode the labels
+    encoder_main = LabelEncoder()
+    encoder_sub = LabelEncoder()
+
+    y_major_enc =  encoder_main.fit_transform(y_major)
+    y_minor_enc =  encoder_sub.fit_transform(y_minor)
+
+    joblib.dump(encoder_main, "enc_main.pkl")
+    joblib.dump(encoder_sub, "enc_sub.pkl")
+    Console().print(f"Saved [cyan]enc_main.pkl[/cyan] and [magenta]enc_sub.pkl[/magenta]")
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    #                      fit on the whole corpus because
+    #               test set will be given separately
+    # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     if parser.t:
         # if tfidf is used then it is an instance of pipeline and should be treated differently
         encoded = tokenizer.fit_transform(x)
+        joblib.dump(tokenizer, "tfidf_tokenizer.pkl")
         joblib.dump(encoded, "tfidf_features.pkl")
         joblib.dump(y_major, "main_labels.pkl")
         joblib.dump(y_minor, "sub_labels.pkl")
@@ -34,7 +70,7 @@ def main():
         encoded = tokenizer.texts_to_sequences(texts=x.tolist())
         # have to do zero padding manually
         encoded = tf.keras.preprocessing.sequence.pad_sequences(encoded, padding="post")
-
+        joblib.dump(tokenizer, "keras_tokenizer.pkl")
         joblib.dump(encoded, "kerasTok_features.pkl")
         joblib.dump(y_major, "main_labels.pkl")
         joblib.dump(y_minor, "sub_labels.pkl")
@@ -43,6 +79,7 @@ def main():
 
     if parser.g:
         encoded = tokenizer(x.tolist(), padding=True, add_special_tokens=False, pad_token=0)
+        joblib.dump(tokenizer, "gpt2_tokenizer.pkl")
         joblib.dump(encoded, "gpt2_features.pkl")
         joblib.dump(y_major, "main_labels.pkl")
         joblib.dump(y_minor, "sub_labels.pkl")
@@ -50,7 +87,9 @@ def main():
             "[cyan]Saved gpt2_features.pkl, main_labels.pkl and sub_labels.pkl in \'command2code\' dir[/cyan]")
 
     if parser.b:
+
         encoded = tokenizer(x.tolist(), padding=True, add_special_tokens=False)
+        joblib.dump(tokenizer, "bert_tokenizer.pkl")
         joblib.dump(encoded, "bert_features.pkl")
         joblib.dump(y_major, "main_labels.pkl")
         joblib.dump(y_minor, "sub_labels.pkl")
