5fba07e90fecb683bb8ac38ba43a678cccccf6ea
Talha Yousuf <talha.yousuf@stech.ai>
2021-06-11 19:42:29 +0500

    added tfidf training lstm without embedding model

--- train.py
+++ train.py
@@ -29,6 +29,7 @@ import shutil
 
 
 flags.DEFINE_bool('t', default=False, help="if given ---> performs training on tfidf features")
+flags.DEFINE_bool('k', default=False, help="if given ---> performs training on keras tokenized features")
 
 
 def main(argv):
@@ -38,20 +39,43 @@ def main(argv):
         config = yaml.load(open("./lstm_torch_tokenizer/conf.yaml", "r"), yaml.SafeLoader)
         args = argparse.Namespace(**config)
 
+        main_enc = joblib.load("enc_main.pkl")  # loading the encoders
+        minor_enc = joblib.load("enc_sub.pkl")  # loading the minor encoders
+        N_classes = len(main_enc.classes_)
+        # update the cfg file accordingly
+        args.classes = N_classes
+        config["classes"] = N_classes # update the yaml file
+
+
     if FLAGS.t:
-        with Console().status("Working with tfidf features", spinner="aesthetic"):
+        with Console().status("Working with tfidf features .... ", spinner="aesthetic"):
             features = joblib.load("tfidf_features.pkl")
-            main_enc = joblib.load("enc_main.pkl") # loading the encoders
-            minor_enc = joblib.load("enc_sub.pkl") # loading the minor encoders
+
             Console().log(f"tfidf features shape ---> {features.A.shape}")
 
-            # update the cfg file accordingly
-            args.
+
 
             model = lstm_embedding_model(args)
             Console().print(model)
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            #             tfidf cannot be used with embedding + lstm
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    if FLAGS.k:
+#         perform training on keras feature-set
+        with Console().status("Working with keras-tokenized-features ....", spinner="aesthetic"):
+            features = joblib.load("kerasTok_features.pkl")
+            Console().log("read the features file")
+            tok = joblib.load("keras_tokenizer.pkl")
+            vocab = len(tok.word_index) + 1
+            args.vocab = vocab
+            config["vocab"] = vocab
+            Console().log("Updated vocabulary size in yaml file ...")
+            yaml.dump(config, open("./lstm_torch_tokenizer/conf.yaml","w"), yaml.SafeDumper)
 
-
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            #         For tfidf features, embedding-layer cannot
+            #                be used hence using lstm_without_embedding_model
+            # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
