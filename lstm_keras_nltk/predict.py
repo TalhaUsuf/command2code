8c0560c4c7f5277fae5af188a43a300ad79f259f
Talha Yousuf <talha.yousuf@stech.ai>
2021-06-13 03:46:58 +0500

    added requirements.txt file

--- lstm_keras_nltk/predict.py
+++ lstm_keras_nltk/predict.py
@@ -5,41 +5,6 @@ from absl import app, flags
 from absl.flags import FLAGS
 from pathlib import Path
 
-<<<<<<< HEAD
-flags.DEFINE_string('cmd', "", 'command to be given')
-
-
-def main(argv):
-    def predict(command_):
-        loaded_model = tf.keras.models.load_model('./models/weights.epoch60-val_acc1.00.h5')
-        tok = joblib.load('tokenizer.pkl')
-        cmd = tok.texts_to_sequences([command_])
-        # no need of zero padding
-        ydecode = joblib.load("label_decode.pkl")
-        ydecode = {a: b for b, a in ydecode.items()}
-        pred = loaded_model.predict(cmd)
-        pred = tf.squeeze(
-            pred, axis=None, name=None
-        )
-        idx = tf.math.argmax(input=pred)
-        idx = idx.numpy().tolist()
-        fname = ydecode[idx]
-
-        return [fname, open(fname, 'r').read()]
-
-    f, id = predict(FLAGS.cmd)
-    print = Console().print
-    print()
-    print()
-    print()
-    print(id)
-    print(f)
-
-
-if __name__ == '__main__':
-=======
-flags.DEFINE_string("cmd", "", "command to be given by the user")
-
 
 def predict(command_):
     """
@@ -92,5 +57,5 @@ def main(argv):
 
 if __name__ == '__main__':
     Console().rule(title="Prediction Script",align="center", style="red on yellow")
->>>>>>> 212227f421381210694f9f85ec80b1f31a59caf8
+# >>>>>>> 212227f421381210694f9f85ec80b1f31a59caf8
     app.run(main)
